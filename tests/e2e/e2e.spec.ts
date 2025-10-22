c6906daf12a5111bfa36c7bd542980d946282870
Leonardo Hernandez <hernandeez00@gmail.com>
2025-10-22 00:14:27 -0300

    change agent pool to windows

--- tests/e2e/e2e.spec.ts
+++ tests/e2e/e2e.spec.ts
@@ -1,20 +1,13 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Login title', async ({ page }) => {
-  await page.goto('/');
+test("Login successfuly", async ({ page }) => {
+  await page.goto("/login.html");
+  await expect(page.getByTestId("login-title")).toContainText("🔐 Login");
 
-  // Click the get started link.
-  await expect(page.getByRole('heading', { name: '🔐 Login' })).toBeVisible();
-});
-
-test('Login successfuly', async ({ page }) => {
-  await page.goto('/login.html');
-  await expect(page.getByTestId('login-title')).toContainText('🔐 Login');
-
-  await page.getByTestId('login-input').click();
-  await page.getByTestId('login-input').fill('admin');
-  await page.getByTestId('password-input').click();
-  await page.getByTestId('password-input').fill('Admin@123');
-  await page.getByTestId('submit-btn').click();
-  await expect(page.getByTestId('session-timer')).toBeVisible();
+  await page.getByTestId("login-input").click();
+  await page.getByTestId("login-input").fill("admin");
+  await page.getByTestId("password-input").click();
+  await page.getByTestId("password-input").fill("Admin@123");
+  await page.getByTestId("submit-btn").click();
+  await expect(page.getByTestId("session-timer")).toBeVisible();
 });
