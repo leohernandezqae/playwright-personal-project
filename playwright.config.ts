a184fe3a4b207e677cc8db38ba7304f78ce5c14f
Leonardo Hernandez <hernandeez00@gmail.com>
2025-10-25 13:23:58 -0300

    artifacts path

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,45 +11,17 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [["list"], ["html", { outputFolder: "./tests/artifacts/test-report" }]],
-
+  reporter: [
+    ["Line"],
+    ["list"],
+    ["html", { outputFolder: "./tests/artifacts/test-report" }]
+  ],
   projects: [
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
-    },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    }
   ],
-
   webServer: {
     command: "npm run start",
     url: process.env.BASE_URL || 'http://localhost:3000',
